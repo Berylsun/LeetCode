ebcd112ed1f93cc118c792fe945216208a575e68
Beryl Sun <sunbwlll@163.com>
2019-05-01 14:18:39 -0700

    Create 71. Simplify Path.java

---
+++ Java/71. Simplify Path.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public String simplifyPath(String path) {
+        String[] str = path.split("/+");
+        Stack<String> stack = new Stack<>();
+        for (String s : str) {
+            if (s.equals("..")) {
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            }else if (!s.equals(".") && !s.equals("")) {
+                stack.push(s);
+            }            
+        }
+        String res = "";
+        while(!stack.isEmpty()) {
+            res = "/" + stack.pop() + res;
+        }
+        if (res.length() == 0) {
+            return "/";
+        }
+        return res;
+    }
+}
