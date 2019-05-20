dfb3d7f859f249412081c1cc4f1ad13ca87522d7
Beryl Sun <sunbwlll@163.com>
2019-05-20 13:57:57 -0700

    Update 389. Find the Difference.java

--- Java/389. Find the Difference.java
+++ Java/389. Find the Difference.java
@@ -11,3 +11,16 @@ class Solution {
         return (char) (tsum - ssum);
     }
 }
+
+
+//O(n), O(1)
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char c = t.charAt(t.length() - 1);
+        for (int i = 0; i < s.length(); i++) {
+            c ^= s.charAt(i);
+            c ^= t.charAt(i);
+        }
+        return  c;
+    }
+}
