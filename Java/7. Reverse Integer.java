3c31cb5c5010b30e08215a4dee3a945c9f542bfd
Beryl Sun <sunbwlll@163.com>
2019-05-20 23:26:14 -0700

    Update 7. Reverse Integer.java

--- Java/7. Reverse Integer.java
+++ Java/7. Reverse Integer.java
@@ -1,13 +1,13 @@
 class Solution {
     public int reverse(int x) {
-        int result = 0;
-        while (x != 0){
-            int tail = x % 10;
-            int newResult = result * 10 + tail;
-            if ((newResult - tail) / 10 != result){ return 0; }
-            result = newResult;
-            x = x / 10;
-        }
-        return result;
+        long res = 0;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x /= 10;
+            if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }        
+        return (int) res;
     }
 }
