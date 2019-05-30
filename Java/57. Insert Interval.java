2046745cf12817fe8fc1063d6c89a1fe19cd2337
Beryl Sun <sunbwlll@163.com>
2019-05-30 14:08:14 -0700

    Update 57. Insert Interval.java

--- Java/57. Insert Interval.java
+++ Java/57. Insert Interval.java
@@ -1,56 +1,19 @@
-class Solution {
-    public int[][] insert(int[][] intervals, int[] newInterval) {
-        if (intervals == null || intervals.length == 0) {
-            int[][] a = new int[1][2];
-            a[0][0] = newInterval[0];
-            a[0][1] = newInterval[1];
-            return a;
-        } 
-        int start = Integer.MIN_VALUE;
-        int end = Integer.MAX_VALUE;
-        ArrayList<int[]> res = new ArrayList<>();
-        for (int i = 0; i < intervals.length; i++) {
-            if (intervals[i][0] <= newInterval[0]) {
-                start = Math.max(start, intervals[i][0]);
-            }
-            if (intervals[i][1] >= newInterval[1]) {
-                end = Math.min(end, intervals[i][1]);
-            }
+public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> res = new ArrayList<>();
+        int i = 0;
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            res.add(intervals[i]);
+            i++;
         }
-        if (start == Integer.MIN_VALUE) start = newInterval[0];
-        if (end == Integer.MAX_VALUE) end = newInterval[1];
-        
-        int count = -1;
-        int index = -1;
-        for (int i = 0; i < intervals.length; i++) {
-            if (start == intervals[i][0]) {
-                if (intervals[i][1] < newInterval[0]){
-                    start = newInterval[0];
-                } 
-            }
-            if (end == intervals[i][1]) {
-                index = i + 1;
-                //count += intervals.length - 1 - i;
-                if (intervals[i][0] > newInterval[1]) {
-                    end = newInterval[1];
-                    index--;
-                }                    
-            }
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
         }
-        int left = 0;
-        while (left < intervals.length && intervals[left][1] < start) {
-            res.add(intervals[left]);
-            left++;
+        res.add(newInterval);
+        while (i < intervals.length) {
+            res.add(intervals[i]);
+            i++;
         }
-        res.add(new int[]{start, end});
-        while (index > -1 && index < intervals.length && intervals[index][0] > end) {
-            res.add(intervals[index]);
-            index++;
-        }
-        int[][] result = new int[res.size()][2];
-        for (int j = 0; j < res.size(); j++) {
-            result[j] = res.get(j);
-        }
-        return result;
+        return res.toArray(new int[][]{});
     }
-}
