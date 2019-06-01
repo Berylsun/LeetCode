dc66b5dedd17ca5f8479aa88d50d8e8535134899
Beryl Sun <sunbwlll@163.com>
2019-05-31 22:01:55 -0700

    Create 134. Gas Station.java

---
+++ Java/134. Gas Station.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sum = 0;
+        int total = 0;
+        int start = 0;
+        for (int i = 0 ; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+            if (sum < 0) {
+                sum = gas[i] - cost[i];
+                start = i;
+            }else {
+                sum += gas[i] -cost[i];
+            }
+        }
+        return total < 0 ? -1 : start;
+    }
+}
