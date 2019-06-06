f0952a153b0bed576dcc02962dd8bf68cf53577d
Beryl Sun <sunbwlll@163.com>
2019-06-05 23:46:11 -0700

    Update 19. Remove Nth Node From End of List.java

--- Java/19. Remove Nth Node From End of List.java
+++ Java/19. Remove Nth Node From End of List.java
@@ -8,51 +8,18 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int length=len(head);
-        ListNode current=head;
-        if(length-n-1>0){
-            for(int i=0;i<length-n-1;i++){
-                current=current.next;
-            }
-            current.next=current.next.next;
-        }else if(length==n){
-            head=head.next;
-        }else{
-            head.next=head.next.next;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
         }
-        
-        return head;
-        
-    }
-    public int len(ListNode head){
-        int a=0;
-        while(head!=null){
-            head=head.next;
-            a++;
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
         }
-        return a;
+        slow.next = slow.next.next;
+        return dummy.next;
     }
-}
-
-
-
-
-public ListNode removeNthFromEnd(ListNode head, int n) {
-    
-    ListNode start = new ListNode(0);
-    ListNode slow = start, fast = start;
-    slow.next = head;
-    
-    //Move fast in front so that the gap between slow and fast becomes n
-    for(int i=1; i<=n+1; i++)   {
-        fast = fast.next;
-    }
-    //Move fast to the end, maintaining the gap
-    while(fast != null) {
-        slow = slow.next;
-        fast = fast.next;
-    }
-    //Skip the desired node
-    slow.next = slow.next.next;
-    return start.next;
 }
