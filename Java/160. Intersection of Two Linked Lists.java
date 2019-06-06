60d47f343848747801ed8a772d620007fad43f77
Beryl Sun <sunbwlll@163.com>
2019-06-06 16:53:06 -0700

    Create 160. Intersection of Two Linked Lists.java

---
+++ Java/160. Intersection of Two Linked Lists.java
@@ -0,0 +1,70 @@
+
+//n, 1
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //统一长度然后比较；
+        int lenA = len(headA);
+        int lenB = len(headB);
+        if (lenA > lenB) {
+            while (lenA != lenB) {
+                headA = headA.next;
+                lenA--;
+            }
+        }else {
+            while (lenA != lenB) {
+                headB = headB.next;
+                lenB--;
+            }
+        }
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+    
+    public int len(ListNode head) {
+        int res = 0;
+        while (head != null) {
+            head = head.next;
+            res++;
+        }
+        return res;
+    }
+}
+
+
+//(m + n), 1
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode A = headA;
+        ListNode B = headB;
+        while (A != B) {
+            A = A == null ? headB : A.next;
+            B = B == null ? headA : B.next;
+        }
+        return A;
+    }
+}
