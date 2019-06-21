94a3178c458d9a17150249f9960fc240593f8b4a
Beryl Sun <sunbwlll@163.com>
2019-06-21 12:31:11 +0800

    Create  252. Meeting Rooms.java

---
+++ Java/252. Meeting Rooms.java
@@ -0,0 +1,10 @@
+//nlogn, 1
+public boolean canAttendMeetings(Interval[] intervals) {
+        Arrays.sort(intervals, (x, y) -> x.start - y.start);
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i - 1].end > intervals[i].start) {
+                return false;
+            }
+        }
+        return true;
+    }
