6a472c6be2cc845c2c83fe210d4ce006ae10d477
Beryl Sun <sunbwlll@163.com>
2019-06-24 20:47:14 +0800

    Create 60. Permutation Sequence.java

---
+++ Java/60. Permutation Sequence.java
@@ -0,0 +1,24 @@
+//n^2, n
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            res.add(i);
+        }
+        int[] fact = new int[n];
+        fact[0] = 1;
+        for (int i = 1; i < n; i++) {
+            fact[i] = i * fact[i - 1];
+        }
+        k = k - 1;
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = n; i > 0; i--) {
+            int index = k/ fact[i - 1];
+            sb.append(res.get(index));
+            k %= fact[i - 1];
+            res.remove(index); //O(n)
+        }
+        return sb.toString();
+    }
+}
