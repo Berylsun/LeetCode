7ac4acabc9fc81acc5301c063c15d0c387b2aca7
Beryl Sun <sunbwlll@163.com>
2019-07-03 22:48:54 +0800

    Update 253. Meeting Rooms II.java

--- Java/253. Meeting Rooms II.java
+++ Java/253. Meeting Rooms II.java
@@ -1,5 +1,8 @@
 class solution {
     public int meeting (Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return 0;
+        }
         int[] starts = new int[intervals.length];
         int[] ends = new int[intervals.length];
         for (int i = 0; i < intervals.length; i++) {
@@ -19,4 +22,24 @@ class solution {
         }
         return res;
     }
+}
+
+
+public int meeting (Interval[] intervals) {
+    if (intervals == null || intervals.length == 0) {
+            return 0;
+        }
+    int res = 0;
+    Arrays.sort(intervals, (a, b) -> a.start - b.start);
+    PriorityQueue<Interval> heap = new PriorityQueue<>((a, b) -> a.end - b.end);
+    for (int i = 0; i < intervals.length; i++) {
+        Interval interval = heap.poll();
+        if (intervals[i].start >= interval.end) {
+            interval.end = intervals[i].end;
+        }else {
+            heap.offer(intervals[i]);
+        }
+        heap.offer(interval);
+    }
+    return heap.size();
 }
