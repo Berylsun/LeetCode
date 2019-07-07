1b2f93f82ddd1d7a8c06c6d7c149d31a294891f2
Beryl Sun <sunbwlll@163.com>
2019-07-07 16:50:24 +0800

    Update 200. Number of Islands.java

--- Java/200. Number of Islands.java
+++ Java/200. Number of Islands.java
@@ -1,4 +1,4 @@
-//m*n, m*n
+//m*n, n
 class Solution {
     public int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0) return 0;
@@ -23,3 +23,47 @@ class Solution {
         dfs(grid, i, j - 1);
     }
 }
+
+//m*n, m*n
+public int numIslands2(char[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    bfs(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    private void bfs(char[][] grid, int x, int y) {
+        grid[x][y] = '0';
+        int n = grid.length;
+        int m = grid[0].length;
+        Queue<Integer> queue = new LinkedList<>();
+        int code = x * m + y;
+        queue.offer(code);
+        while (!queue.isEmpty()) {
+            code = queue.poll();
+            int i = code / m;
+            int j = code % m;
+            if (i > 0 && grid[i - 1][j] == '1') {
+                queue.offer((i - 1) * m + j);
+                grid[i - 1][j] = '0';
+            }
+            if (i < n - 1 && grid[i + 1][j] == '1') {
+                queue.offer((i + 1) * m + j);
+                grid[i + 1][j] = '0';
+            }
+            if (j > 0 && grid[i][j - 1] == '1') {
+                queue.offer((i * m) + j - 1);
+                grid[i][j - 1] = '0';
+            }
+            if (j < m - 1 && grid[i][j + 1] == '1') {
+                queue.offer((i * m) + j + 1);
+                grid[i][j + 1] = '0';
+            }
+        }
+    }
