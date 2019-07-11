b06cc4973f70a5136d51debaa23b9b0399fc7bfc
Beryl Sun <sunbwlll@163.com>
2019-07-11 15:20:45 +0800

    Update 350. Intersection of Two Arrays II.java

--- Java/350. Intersection of Two Arrays II.java
+++ Java/350. Intersection of Two Arrays II.java
@@ -1,24 +1,52 @@
-class Solution {
-    public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map=new HashMap<>();
-        List<Integer> res= new ArrayList<>();
-        for(int i=0;i<nums1.length;i++){
-            if(map.containsKey(nums1[i])){
-                map.put(nums1[i],map.get(nums1[i])+1);
-            }else{
-                map.put(nums1[i],1);
+// HashMap, time : O(n), space : O(n);
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < nums1.length; i++) {
+            if (map.containsKey(nums1[i])) {
+                map.put(nums1[i],map.get(nums1[i]) + 1);
+            } else {
+                map.put(nums1[i], 1);
             }
         }
-        for(int i=0;i<nums2.length;i++){
-            if(map.containsKey(nums2[i])&&map.get(nums2[i])>0){
-                res.add(nums2[i]);
-                map.put(nums2[i],map.get(nums2[i])-1);
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i])) {
+                if (map.get(nums2[i]) > 0) {
+                    ret.add(nums2[i]);
+                    map.put(nums2[i], map.get(nums2[i]) - 1);
+                }
             }
         }
-        int[] ans=new int[res.size()];
-        for(int i=0;i<ans.length;i++){
-            ans[i]=res.get(i);
+        int[] res = new int[ret.size()];
+        int k = 0;
+        for (Integer num : ret) {
+            res[k++] = num;
         }
-        return ans;
+        return res;
     }
-}
+
+    // Arrays.sort time : O(nlogn) space : O(n);
+    public static int[] intersect2(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> ret = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                ret.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] res = new int[ret.size()];
+        int k = 0;
+        for (Integer num : ret) {
+            res[k++] = num;
+        }
+        return res;
+    }
