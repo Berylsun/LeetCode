d094c1f810dd3e6577fd102ee35bc0b4492250dd
Beryl Sun <sunbwlll@163.com>
2019-07-12 00:09:27 +0800

    Create 70. Climbing Stairs.java

---
+++ Java/70. Climbing Stairs.java
@@ -0,0 +1,21 @@
+//2^n, n
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        } else {
+            return climbStairs(n - 1) + climbStairs(n - 2);
+        }
+    }
+    
+    
+//n, 1
+    public int climbStairs2(int n) {
+        if (n <= 1) return 1;
+        int oneStep = 1, twoStep = 1, res = 0;
+        for (int i = 2; i <= n; i++) {
+            res = oneStep + twoStep;
+            twoStep = oneStep;
+            oneStep = res;
+        }
+        return res;
+    }
