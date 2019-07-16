44ea3d3da4c83acdb8a58519aaa9fe9dac849031
Beryl Sun <sunbwlll@163.com>
2019-07-16 23:09:54 +0800

    Update 2.1 数据类型+Int&Integer.java

--- Tips/2.1 数据类型+Int&Integer.java
+++ Tips/2.1 数据类型+Int&Integer.java
@@ -17,10 +17,20 @@ x.equals(y) -> true;
 x == y -> false
 当x, y属于-128-127之间时，x==y -> true
 
-//string -> Integer/long/……
+//string -> int/long/……
 String s = "123";
 Integer.parseInt(s);
 Long.parseLong(s);
 
 //Integer/long/…… -> String
 x.toString();
+
+
+Integer.valueOf(s) 
+多次解析相同的一个字符串时，得到的是Integer类型的对象，得到的对象有时是同一个对象，有时是不同的对象，
+要根据把s字符串解析的整数值的大小进行决定：
+如果s字符串对应的整数值在 -128~127之间，则解析出的Integer类型的对象是同一个对象；
+如果s字符串对应的整数值不在-128~127之间，则解析出的Integer类型的对象不是同一个对象。不管对象是否相等，对象中的value值是相等的。
+
+Integer.parseInt(s)是把字符串解析成int基本类型，
+Integer.valueOf(s)是把字符串解析成Integer对象类型，其实int就是Integer解包装，Integer就是int的包装，
