85ae0d84f98cba5341a4cdf259b15c996565cc9f
Beryl Sun <sunbwlll@163.com>
2019-08-13 18:46:07 +0800

    cc

--- Java/171. Excel Sheet Column Number.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int titleToNumber(String s) {
-        int res = 0;
-        for (char c : s.toCharArray()) {
-            res = res * 26 + c - 'A' + 1;
-        } 
-        return res;
-    }
-}
--- Java/319. Bulb Switcher.java
+++
@@ -1,5 +0,0 @@
-class Solution {
-    public int bulbSwitch(int n) {
-        return (int)Math.sqrt(n); //1,4,9,16,……
-    }
-}
