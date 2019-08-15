1eab83814dba1c1afbf73aa5598eb88166c8f402
Beryl Sun <sunbwlll@163.com>
2019-08-15 22:20:32 +0800

    Update 276. Paint Fence.java

--- Important_250/276. Paint Fence.java
+++ Important_250/276. Paint Fence.java
@@ -14,3 +14,19 @@ public int numWays(int n, int k) {
     }
     return same + diff;
 }
+
+
+public int numWays(int n, int k) {
+        if(n == 0) return 0;
+        if(n == 1) return k;
+        int[] dp = new int[n];
+        dp[0] = k;
+        dp[1] = k*(k-1);
+        int[] dp2 = new int[n];
+        dp2[1] = k; 
+        for(int i = 2; i < n; i++){
+            dp2[i] = dp[i-1];
+            dp[i] = (dp[i-1]+dp2[i-1])*(k-1);
+        }
+        return dp[n-1]+dp2[n-1];
+    }
