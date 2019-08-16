b42c4e1b250dc02eb5286f4eabae1a2e18d6a201
Beryl Sun <sunbwlll@163.com>
2019-08-16 15:56:40 +0800

    Create 283. Move Zeroes.java

---
+++ Important_250/283. Move Zeroes.java
@@ -0,0 +1,14 @@
+//n, 1
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = 0;
+                nums[idx++] = temp;                
+            }
+        }
+    }
+}
