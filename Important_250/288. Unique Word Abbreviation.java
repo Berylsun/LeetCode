04035727f6aa0300fd5f14cffb18d763b70a3f91
Beryl Sun <sunbwlll@163.com>
2019-08-16 17:21:36 +0800

    Create 288. Unique Word Abbreviation.java

---
+++ Important_250/288. Unique Word Abbreviation.java
@@ -0,0 +1,25 @@
+//
+
+HashMap<String, String> map;
+public _288_UniqueWordAbbreviation(String[] dictionary) {
+    map = new HashMap<>();
+    for (String s : dictionary) {
+        String key = getKey(s);
+        if (map.containsKey(key)) {
+            if (!map.get(key).equals(s)) {
+                map.put(key, ""); //不能remove 因为如果出现三个的话……
+            }
+        }else {
+            map.put(key, s);
+        }
+    }    
+}
+
+public boolean isUnique(String word) {
+        return !map.containsKey(getKey(word)) || map.get(getKey(word)).equals(word);
+}
+
+private String getKey(String s) {
+    if (s.length() <= 2) return s;
+    return s.charAt(0) + Integer.toString(s.length() - 2) + s.charAt(s.length() - 1);
+}
