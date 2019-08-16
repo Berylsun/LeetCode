274fc54289d3294bc8c7b27047b69fbfa29957ab
Beryl Sun <sunbwlll@163.com>
2019-08-16 17:43:03 +0800

    Create 289. Game of Life.java

---
+++ Important_250/289. Game of Life.java
@@ -0,0 +1,41 @@
+//m*n, 1
+class Solution {
+    public void gameOfLife(int[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) return;
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                int count = countNeighbor(board, i, j);
+                if (board[i][j] == 1) {
+                    if (count == 2 || count == 3) {
+                        board[i][j] +=2;
+                    }
+                }else {
+                    if (count == 3){
+                        board[i][j] += 2;
+                    }
+                }
+            }
+        }
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] >>= 1;
+            }            
+        }
+    }
+    
+    public int countNeighbor(int[][] board, int i, int j) {
+        int count = 0;
+        for (int row = Math.max(0, i - 1); row <= Math.min(board.length - 1, i + 1); row++) {
+            for (int col = Math.max(0, j - 1); col <= Math.min(board[0].length - 1, j + 1); col++) {
+                if (row == i && col == j) continue; //
+                if ((board[row][col] & 1) == 1) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
