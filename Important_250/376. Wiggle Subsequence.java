ad57d0b64da03609e4af6a469f6e3015c11e131e
Beryl Sun <sunbwlll@163.com>
2019-08-22 22:49:20 +0800

    Create 376. Wiggle Subsequence.java

---
+++ Important_250/376. Wiggle Subsequence.java
@@ -0,0 +1,44 @@
+//n, n
+DP!!!
+class Solution {
+    public int wiggleMaxLength(int[] nums) {
+        if (nums.length < 2) return nums.length;
+        int[] up = new int[nums.length];
+        int[] down = new int[nums.length];
+        up[0] = down[0] = 1;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up[i] = down[i - 1] + 1;
+                down[i] = down[i - 1];
+            }else if (nums[i] < nums[i - 1]) {
+                down[i] = up[i - 1] + 1;
+                up[i] = up[i - 1];
+            }else {
+                down[i] = down[i - 1];
+                up[i] = up[i - 1];
+            }
+        }
+        
+        return Math.max(down[nums.length - 1], up[nums.length - 1]);
+    }
+}
+
+//n, 1
+class Solution {
+    public int wiggleMaxLength(int[] nums) { 
+        if (nums.length < 2) return nums.length;
+        int up = 1;
+        int down = 1;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up = down + 1;
+            }else if (nums[i] < nums[i - 1]) {
+                down = up + 1;
+            }
+        }
+        
+        return Math.max(up, down);
+    }
+}
