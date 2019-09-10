640f15a56002d819f55ebce996e6a37090c4070a
Beryl Sun <sunbwlll@163.com>
2019-09-10 13:28:48 +0800

    Update 26. Remove Duplicates from Sorted Array.java

--- Important_250/26. Remove Duplicates from Sorted Array.java
+++ Important_250/26. Remove Duplicates from Sorted Array.java
@@ -2,7 +2,7 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
-        int count = 1;
+        int count = 1; // count can also be index of new nums
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[i - 1]) {  //reverse thinking
                 nums[count++] = nums[i];
