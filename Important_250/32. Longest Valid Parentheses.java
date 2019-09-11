53749c4559bb91eab8c9acce3498da591ff1962b
Beryl Sun <sunbwlll@163.com>
2019-09-11 16:46:24 +0800

    Update 32. Longest Valid Parentheses.java

--- Important_250/32. Longest Valid Parentheses.java
+++ Important_250/32. Longest Valid Parentheses.java
@@ -4,7 +4,7 @@ class Solution {
         if (s == null || s.length() == 0) return 0;
         int start = -1;
         int res = 0;
-        Stack<Integer> stack = new Stack<>(); //must be Integer instead of Character
+        Stack<Integer> stack = new Stack<>(); //must be Integer instead of Character   OR ("()(())") will be wrong!!!
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
                 stack.push(i);
