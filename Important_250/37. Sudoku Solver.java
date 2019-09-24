204bfc3df78dd1119b1c3bffbea2db150692129a
Beryl Sun <sunbwlll@163.com>
2019-09-24 13:20:21 +0800

    Update 37. Sudoku Solver.java

--- Important_250/37. Sudoku Solver.java
+++ Important_250/37. Sudoku Solver.java
@@ -4,7 +4,7 @@ class Solution {
         if (board == null || board.length == 0) return;
         solve(board);
     }
-    
+    // be brave!!!
     public boolean solve(char[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
