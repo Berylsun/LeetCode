ed242b35546d9c7f5c0e1268c098147f986a2649
Beryl Sun <sunbwlll@163.com>
2021-01-15 22:42:31 +0800

    rearrange

--- Java/2019/175. Combine Two Tables.java
+++
@@ -1,4 +0,0 @@
-# Write your MySQL query statement below
-SELECT Person.FirstName, Person.LastName, Address.City, Address.state FROM 
-Person LEFT JOIN Address
-ON Person.PersonId = Address.PersonId;
