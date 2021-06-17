3f88cfb234aa632af578120c734fde05a350e5ff
sunbowen.beryl21 <sunbowen.beryl21@bytedance.com>
2021-06-17 11:41:22 +0800

    --

---
+++ Go/MergeIntervals56.go
@@ -0,0 +1,38 @@
+package Go
+
+import "sort"
+
+//O(mn) O(mn)
+func merge(intervals [][]int) [][]int {
+	if intervals == nil || len(intervals) == 0 || intervals[0] == nil || len(intervals[0]) == 0 {
+		return intervals
+	}
+	sort.Slice(intervals, func (i, j int) bool{
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	start := intervals[0][0]
+	end := intervals[0][1]
+	idx := 1
+	res := [][]int{}
+	for idx < len(intervals) {
+		interval := intervals[idx]
+		if interval[0] <= end {
+			end = max(interval[1], end)
+		} else if interval[0] > end {
+			res = append(res, []int{start, end})
+			start = interval[0]
+			end = interval[1]
+		}
+		idx++
+	}
+	res = append(res, []int{start, end})
+	return res
+}
+
+func max(x, y int) int{
+	if x > y {
+		return x
+	}
+	return y
+}
