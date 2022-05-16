413a4c526ad1073204b9e9b5375c5354929a63fb
sunbowen.beryl21 <sunbowen.beryl21@bytedance.com>
2022-05-16 11:09:30 +0800

    733

--- Tag/DFS_BFS/733_FloodFill.java
+++ Tag/DFS_BFS/733_FloodFill.java
@@ -23,4 +23,30 @@ class Solution {
         helper(image, i, j + 1, newColor, color);
         helper(image, i, j - 1, newColor, color);
     }
+
+    public int[][] floodFill2(int[][] image, int sr, int sc, int newColor) {
+        int[][] dir = {{0,-1},{0,1},{-1,0},{1,0}};
+        int oldColor = image[sr][sc];
+        if (oldColor == newColor) {
+            return image;
+        }
+        int m = image.length;
+        int n = image[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        image[sr][sc] = newColor;
+        queue.offer(new int[]{sr, sc});
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            for (int j = 0; j < 4; j++) {
+                int x = cur[0] + dir[j][0];
+                int y = cur[1] + dir[j][1];
+                if (x < 0 || x >= m || y < 0 || y >= n || image[x][y] != oldColor) {
+                    continue;
+                }
+                image[x][y] = newColor;
+                queue.offer(new int[]{x, y});
+            }
+        }
+        return image;
+    }
 }
